63ae4fed68edd60247cc4717a721017057975e95
alejandroabrilm <d.abrilm@uniandes.edu.co>
2023-08-30 21:43:38 -0500

    VIVA

--- Proyecto0.py
+++ Proyecto0.py
@@ -13,8 +13,34 @@ def corrector(lista):
     expresion = expresion.strip()
     expresion = expresion.lowercase()
     return expresion
-    
-    
+
+def correctorDefinicioneVariables(lista):
+    respuestaFinal = False
+    listaDefiniciones = ["defvar", "defproc"]
+    respuesta = []
+    for i in range(len(lista)):
+        expresion = corrector(lista[i])
+        if listaDefiniciones[0] in expresion:
+            x = expresion.split(" ")
+            if len(x == 3):
+                respuesta.append(True)
+            else:
+                respuesta.append(False)
+        if listaDefiniciones[1] in expresion:
+            x = expresion.split(" ")
+            if len(x == 3):
+                if "(" in x[2] and ")" in x[2] and "," in x[2]:
+                    if x[2].find("(") < x[2].find(")") and x[2].count(",") == 1:
+                        respuesta.append(True) 
+                else:
+                    respuesta.append(False)
+            else:
+                respuesta.append(False)
+    if respuesta.size() == lista.size():
+        respuestaFinal = True
+    return respuestaFinal
+                
+      
 def lexer(lista):
     listaDefiniciones = ["jump","walk","leap", "turn", "turnto", "drop", "get", "grab", "letgo", "nop"]
     listaAbreviaciones = ["ju","wa","le","tu","tt", "dr", "ge", "gr", "lg", "np"]
@@ -24,5 +50,4 @@ def lexer(lista):
         for char in range(len(listaDefiniciones)):
             if expresion in char():
                 listaFinal.append(expresion.split(" "))
-                                
                 
