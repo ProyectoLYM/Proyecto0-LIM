c0169d640f5a742ec9f0d87b953005d476a208ca
Dav1d10 <d.carol@uniandes.edu.co>
2023-09-03 13:11:39 -0500

    cambios

--- Proyecto0.py
+++ Proyecto0.py
@@ -13,6 +13,48 @@ def cargar_archivo(archivo):
 print(cargar_archivo("ejemploFull.txt"))
 
 
+def definicionVariable(linea):
+    if "defVar" in linea:
+        palabra = linea.split(" ")
+        if "defVar" in palabra[0]:
+            if len(palabra) == 3:
+                primer_token = "Variable"
+                segundo_token = "Nombre"
+                tercer_token = "Valor"
+                token = (primer_token,segundo_token,tercer_token)
+            if len(palabra) > 3 or len(palabra) < 3:
+                token = "No valido"
+        else:
+            token = "No valido"
+    return token 
+            
+
+def definicionProceso(linea):
+    if "defProc" in linea:
+        palabra = linea.split(" ")
+        if "defProc" in palabra[0]:
+            if len(palabra[1] != 0):
+                if "(" and ")" in linea:
+                    parametros = linea.split("(")
+                    parametro = parametros.split(",")
+                    if parametro > linea.count(","):
+                        token = ("Proceso", "Nombre", "Parametros")
+                else:
+                    token = "No valido"
+            else:
+                token = "No valido"
+        else:
+            token = "No valido"
+    return token
+
+
+def condicional(linea):
+    if "if" in linea or "else" in linea:
+            palabra = linea.split(" ")
+            if "if" in palabra[0]:
+                if "{" in linea or "{" in lineas[i + 1]:
+
+
 def lexer(contenido):
     lineas = contenido.split("\n")
     tokens = []
