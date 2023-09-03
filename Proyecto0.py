e00ba5fa324cdaace63bef02490967bebe510347
Dav1d10 <d.carol@uniandes.edu.co>
2023-09-03 14:04:57 -0500

    Cambios

--- Proyecto0.py
+++ Proyecto0.py
@@ -6,8 +6,7 @@ def quitar_espacios(expresion):
 
 def cargar_archivo(archivo):
     contenido = open(archivo,"r").read()
-    lista = lexer(contenido)
-    return lista
+    return contenido
     
     
 print(cargar_archivo("ejemploFull.txt"))
@@ -53,6 +52,46 @@ def condicional(linea):
             palabra = linea.split(" ")
             if "if" in palabra[0]:
                 if "{" in linea or "{" in lineas[i + 1]:
+                    
+                    
+def parser(contenido):
+    respuesta1 = ""
+    respuesta2 = ""
+    respuesta3 = ""
+    correcto = ""
+    lineas = contenido.split("\n")
+    for i in range(len(lineas)):
+        linea = quitar_espacios(lineas[i])
+    if definicionVariable(linea) == "Valido":
+        respuesta1 = True
+    else:
+        respuesta1 = False
+    if definicionProceso(linea) == "Valido":
+        respuesta2 = True
+    else:
+        respuesta2 = False
+    if condicional(linea) == "Valido":
+        respuesta3 = True
+    else:
+        respuesta3 = False
+    if respuesta1 and respuesta2 and respuesta3 == True:
+        correcto = True
+    else:
+        correcto = False    
+    return correcto 
+
+
+def resultado(archivo):
+    archivo = "ejemploFull.txt"
+    carga = cargar_archivo(archivo)
+    respuesta = parser(carga)
+    if respuesta == True:
+        res = print("La sintaxis es correcta: True")
+    else:
+        res = print("La sintaxis no es correcta: False")
+    return res
+    
+    
 
 
 def lexer(contenido):
